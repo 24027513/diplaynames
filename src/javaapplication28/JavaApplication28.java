7db91e96922554dd8fcf4a39c49f7a60612a7dbf
24027513 <163356727+24027513@users.noreply.github.com>
2024-08-29 23:13:06 +0200

    my first commit

---
+++ src/javaapplication28/JavaApplication28.java
@@ -0,0 +1,17 @@
+
+package javaapplication28;
+
+public class JavaApplication28 {
+
+    public static void main(String[] args) {
+      
+        String[] names = {"Rathari", "Mathux", "Junior" };
+        int[] age = {18, 19, 17};
+        
+        
+        System.out.println(names[1] +" " +age[2]);
+        
+        
+    }
+    
+}
